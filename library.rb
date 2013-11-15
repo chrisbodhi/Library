1c20ccd00d72b99e238cb636aa985422a65689bb
Chris Boette <cboette@gmail.com>
2013-11-15 14:44:37 -0600

    Add methods for available_books, list_books, borrowed_books; notes for borrowed_books_count, borrowed_books, check_out

--- library.rb
+++ library.rb
@@ -1,4 +1,5 @@
 class Library
+
   def initialize
     @books = []
   end
@@ -7,18 +8,32 @@ class Library
   end
 
   def list_books
+    @books.each do |book|
+      print book.title + ' - ' + book.status
+      puts ''
+    end
   end
 
   def borrowed_books
+    @books.each do |book|
+      puts book.title + ' ' + book.borrower if book.status == "checked_out"
+    end
   end
 
   def available_books
+    @books.each do |book|
+      puts book.title if book.status == "available"
+    end
   end
 
   def add_book(book)
+    @books << book
   end
 
   def check_out(user, book)
+    # @user = user
+    # @book = book
+    #allow if status == available
   end
 
   def check_in(book)
@@ -27,15 +42,21 @@ end
 
 class Borrower
   def initialize(name)
+    @name = name
   end
 
   def borrowed_books
+    # borrowed = []
+
+    # return borrowed
   end
 
   def name
   end
 
   def borrowed_books_count
+    # max checked out is 2
+    # puts borrowed_books.length
   end
 
   def borrowed_books_list
@@ -43,6 +64,14 @@ class Borrower
 end
 
 class Book
+  attr_reader :title, :author
+  attr_accessor :status, :borrower
+
   def initialize(title, author)
+    @title = title
+    @author = author
+    @status = "available"
+    @borrower = nil
   end
+
 end

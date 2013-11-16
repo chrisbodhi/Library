bfb2239d76826f7e7a12d8ec486b678330322b1d
Chris Boette <cboette@gmail.com>
2013-11-15 18:59:58 -0600

    Update the book_review method to handle errors. Add documentation for all but the last method in the Library class.

--- library.rb
+++ library.rb
@@ -3,7 +3,17 @@ class Library
   def initialize
     @books = []
   end
-
+  
+  # Public: Prints the title and status of all books in the library then moves 
+  # to a new line using puts ''.
+  #
+  # Examples
+  #
+  #   list_books
+  #   # => The Stranger - available
+  #   # => Nausea - available
+  #
+  # Returns nil.
   def list_books
     @books.each do |book|
       print book.title + ' - ' + book.status
@@ -11,30 +21,71 @@ class Library
     end
   end
 
+  # Public: Puts the title and borrower of books checked out of the library.
+  #
+  # Examples
+  #
+  #   borrowed_books
+  #   # => The Stranger - Mike
+  #   # => Nausea - Mike
+  #
+  # Returns nil.
   def borrowed_books
     @books.each do |book|
       puts book.title + ' - ' + book.borrower if book.status == "checked_out"
     end
   end
 
+  # Public: Puts the title of all available books in the library.
+  #
+  # Examples
+  #
+  #   available_books
+  #   # => The Stranger
+  #   # => Nausea
+  #
+  # Returns nil.
   def available_books
     @books.each do |book|
       puts book.title if book.status == "available"
     end
   end
 
+  # Public: Adds a book object to the library.
+  #
+  # book - initialized Object from the Book Class
+  #
+  # Examples
+  #
+  #   add_book(stranger)
+  #
+  # Returns nil.
   def add_book(book)
     @books << book
   end
 
+  # Public: Checks a book out to a user if the book is available and if the user has fewer than two books checked out. The book status is changed, the book is added to the user's list of checked out books, and the book's borrower attribute is changed to the user's name. 
+  #
+  # user - initialized Object from the Borrower Class
+  # book - initialized Object from the Book Class
+  #
+  # Examples
+  #
+  #   check_out(mike, stranger)
+  #
+  #   check_out(gilbert, stranger)
+  #   # => Sorry Gilbert, The Stranger is checked out.
+  #   
+  #   check_out(mike, feynman)
+  #   # => Sorry, Mike, you already have two books out.
+  #
+  # Returns nil.
   def check_out(user, book)
     if book.status == "available"
       if user.checked_out_books.length < 2
         user.checked_out_books << book
-        # puts user.checked_out_books
         book.status = "checked_out"
         book.borrower = user.name
-        puts book.title + ' - ' + book.status
       else
         puts "Sorry, #{user.name}, you already have two books out. "
       end
@@ -43,29 +94,58 @@ class Library
     end
   end
 
+  # Public: If a book has been checked out, the status is changed to available.
+  #
+  # book - initialized Object from the Book Class
+  #
+  # Examples
+  #
+  #   check_in(stranger)
+  #
+  # Returns nil.
   def check_in(book)
     if book.status == "checked_out"
       book.status = "available"
     end
   end
 
+  # Public: Prompts the user to submit a rating (from 0 to 5) for the book.
+  #
+  # book - initialized Object from the Book Class
+  #
+  # book.rating is an array of [average rating, total ratings]
+  #
+  # Examples
+  #
+  #   check_in(stranger)
+  #
+  # Returns the rating in the form of an Array of two numbers.
   def book_review(book)
-    puts "Please rate this title, from 0 to 5 stars."
+    puts "Please rate this title, from 1 to 5 stars."
     puts "The current average review is #{book.rating[0]}, which is out of #{book.rating[1]} reviews."
-    puts "0 - 5?"
+    puts "1 - 5?"
+
     rate = gets.chomp
+
     rate = rate.to_i
-    #rating = [average rating, total ratings]
-    total = (book.rating[0] + rate)
-    book.rating[0] = total / (book.rating[1] + 1)
-    book.rating[1] += 1
-    puts "Thanks! The average review is now #{book.rating[0]}, which is out of #{book.rating[1]} reviews."
+
+    if rate.between?(1,5)
+      total = (book.rating[0] + rate)
+      book.rating[0] = total / (book.rating[1] + 1)
+      book.rating[1] += 1
+      puts "Thanks! The average review is now #{book.rating[0]}, which is out of #{book.rating[1]} reviews."
+    else
+      puts "That's no good. Try again."
+      book_review(book)
+    end
+
     book.rating
   end
 
 end
 
 class Borrower
+
   attr_reader :name
   attr_accessor :checked_out_books
 
@@ -89,6 +169,7 @@ class Borrower
 end
 
 class Book
+
   attr_reader :title, :author
   attr_accessor :status, :borrower, :rating
 
